287f5b5534fc9772e8ff72c055c884db14cfb4e1
anjali <anjalisharm7496@email.com>
2023-05-26 17:20:08 +0530

    first commit

---
+++ resources/views/master.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E_Commerce Project</title>
+    <!--Jquery Link-->
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <!-- connect the page header,footer -->
+    {{View::make('header')}}
+
+   @yield('content') 
+
+   {{View::make('footer')}}
+</body>
+<style>
+
+    .custom-login{
+        height: 500px;
+        padding-top:100px;
+    }
+</style>
+</html>
